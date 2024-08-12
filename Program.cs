3199f619011355e236384b671b8165fab4385633
pravin gaikwad <pravingaikwad.sibar@outlook.com>
2024-08-12 14:47:14 +0530

    Assignment Questions added

--- Program.cs
+++ Program.cs
@@ -1,33 +1,42 @@
-﻿
-using nsp1;
+﻿using nsp1;
 
 namespace nsp1
 {
+
     public class classP
     {
         public int x;
         public int y;
         public void display()
-        { 
+        {
         }
        virtual  public void show()
         {
             Console.WriteLine("show from classP ####");
         }
     }
-    public class classC:classP
+    public class classC : classP
     {
-      public  int z;
-       override  public void show()
+        public int z;
+       override public void show()
         {
             Console.WriteLine("show from classC *******");
+
+        }
+    }
+    public class classD : classP
+    {
+        public int z;
+       override public void show()
+        {
+            Console.WriteLine("show from classD @@@@@");
         }
     }
 }
 
 namespace Method_overriding_June_120824
 {
-   
+
     internal class Program
     {
         static void Main(string[] args)
@@ -42,12 +51,24 @@ namespace Method_overriding_June_120824
             //nsp1.classP obj2 = new nsp1.classP();
             //obj2.show();
 
-            nsp1.classP obj3 = new nsp1.classC();
-            //ref obj3 classP(parent)  object child
-            obj3.show();
-            //obj3.display();
-          
+            //nsp1.classP obj3 = new nsp1.classC();///////////1
+            ////ref obj3 classP(parent)  object child
+            //obj3.show();//
+            //obj3 = new nsp1.classD(); /////////////////2
+            //obj3.show();
+
+            //obj3 = new nsp1.classP();/////////////////3
+
+            //obj3.show();
+            classP objP = new classP();
+            classC objC = new classC();
+            classD objD = new classD();
+            temp tt = new temp();
+            tt.callShow(objP);
+            tt.callShow(objC);
+            tt.callShow(objD);
+
         }
     }
-    
+
 }
---
+++ temp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using nsp1;
+namespace Method_overriding_June_120824
+{
+    internal class temp
+    {
+                            //ref variable
+        public void callShow(classP obj)//objects -->classC,C
+        {
+            obj.show();// parents show  or child C show() or child D show()
+        }
+    }
+}
